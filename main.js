962b85ffb5c52050404b2f211eeeb4caa9534576
su <111879114+sucodemaracuj4@users.noreply.github.com>
2024-03-27 09:01:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2023-10-05T00:00:00");
+contadores [0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = newDate();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dia = Math.floor(horas/24);
+}
