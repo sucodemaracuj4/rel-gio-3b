8530e594ad2780507d360ef4124b8904b426ad80
su <111879114+sucodemaracuj4@users.noreply.github.com>
2024-04-01 11:06:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,8 +14,12 @@ textos[i].classList.add("ativo");
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2023-10-05T00:00:00");
-contadores [0].textContent = calculaTempo(tempoObjetivo1);
+const tempoObjetivo2 = new Date ("2023-12-05t00:00:00");
+const tempoObjetivo3 = new Date ("2023-12-30t00:00:00");
+const tempoObjetivo4 = new Date ("2024-02-01t00:00:00"); 
 
+contadores [0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = newDate();
 let tempoFinal = tempoObjetivo1 - tempoAtual;
@@ -23,4 +27,14 @@ let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dia = Math.floor(horas/24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+if (tempoFinal > 0){
+    return dias + " dias " + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    } else{
+        
+    }
 }
